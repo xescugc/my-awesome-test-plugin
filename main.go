b5903dc0997eb26d0980526338db818904a9178c
Ivan Jozic <ivan.jozic@cycloid.io>
2025-12-01 17:00:50 +0100

    main: added setting port from env value

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,8 +14,13 @@ func main() {
 	http.HandleFunc("DELETE /_cy/plugin", plugin)
 	http.HandleFunc("POST /_cy/resync", resync)
 
-	fmt.Println("Server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server is running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
